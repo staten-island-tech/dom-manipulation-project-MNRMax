967b03399b6511e92af91069d400f9309aea3444
MNRMax <88982841+MNRMax@users.noreply.github.com>
2022-10-26 14:00:25 -0400

    adsfa

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 DOMSelectors = {
-    button: document.getElementById("submit"),
-    input: document.getElementById("input"),
+    submit: document.getElementById("submit"),
+    nameInput: document.getElementById("name-input"),
+    titleInput: document.getElementById("title-input"),
+    imageInput: document.getElementById("image-input"),
     objects: document.getElementById("objects"),
-    remove: document.getElementById('remove')
 }
 
-DOMSelectors.button.addEventListener('click', function() {
-    DOMSelectors.objects.insertAdjacentHTML("afterbegin", `<p>${DOMSelectors.input.value}</p>\n`);
-    DOMSelectors.input.value = ''
-});
-DOMSelectors.remove.addEventListener('click', function() {
-    newP = DOMSelectors.objects.innerHTML.split('\n')
-    newP.shift()
-    DOMSelectors.objects.innerHTML = newP.join('\n')
+DOMSelectors.submit.addEventListener('click', function() {
+    let div = document.createElement('div');
+    div.className = 'object'
+    let image = document.createElement('img');
+    let p = document.createElement('p')
+    let h1 = document.createElement('h1')
+    p.innerHTML = DOMSelectors.nameInput.value;
+    h1.innerHTML = DOMSelectors.titleInput.value;
+    image.src = DOMSelectors.imageInput.value;
+    div.appendChild(h1)
+    div.appendChild(p)
+    div.appendChild(image)
+    DOMSelectors.objects.appendChild(div)
 });
