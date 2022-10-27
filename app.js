75d07fc0d5758e6a21968de6e08e6e6c18f4202f
MNRMax <88982841+MNRMax@users.noreply.github.com>
2022-10-27 13:42:32 -0400

    sdfgdf

--- app.js
+++ app.js
@@ -5,34 +5,6 @@ DOMSelectors = {
     imageInput: document.getElementById("image-input"),
     objects: document.getElementById("objects"),
 }
-
-// DOMSelectors.submit.addEventListener('click', function() {
-//     let div = document.createElement('div');
-//     div.className = 'object'
-//     let image = document.createElement('img');
-//     image.className = 'object-image'
-//     let p = document.createElement('p')
-//     let h2 = document.createElement('h2')
-//     let button = document.createElement('button')
-//     p.innerHTML = DOMSelectors.nameInput.value;
-//     h2.innerHTML = DOMSelectors.titleInput.value;
-//     image.src = DOMSelectors.imageInput.value;
-//     button.onclick = function() {
-//         button.parentElement.remove()
-//     }
-//     button.className = "remove-button"
-//     button.innerHTML = "Remove"
-//     div.appendChild(h2)
-//     div.appendChild(p)
-//     div.appendChild(image)
-//     div.appendChild(document.createElement('br'))
-//     div.appendChild(button)
-//     DOMSelectors.objects.appendChild(div)
-//     DOMSelectors.nameInput.value = '';
-//     DOMSelectors.titleInput.value = '';
-//     DOMSelectors.imageInput.value = '';
-// });
-
 DOMSelectors.submit.addEventListener('click', function() {
     p = DOMSelectors.nameInput.value;
     h2 = DOMSelectors.titleInput.value;
@@ -45,7 +17,6 @@ DOMSelectors.submit.addEventListener('click', function() {
                 <img class="object-image" src="${image}">
                 <br>
                 <button class="remove-button" onclick="this.parentElement.remove()">Remove</button>
-        </div>
         </div>`
     )
     DOMSelectors.nameInput.value = '';
